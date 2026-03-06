d833590a08bca7a0ad14c6ffa422c3b29bd1ee9b
pawan Sharma <130146893+pawan-pandit@users.noreply.github.com>
2026-03-07 04:07:00 +0530

    front

--- new/app/components/Hero/page.tsx
+++ new/app/components/Hero/page.tsx
@@ -5,6 +5,8 @@ import Image from "next/image"
 import gsap from "gsap"
 import { ScrollTrigger } from "gsap/all"
 import { motion, Easing } from "framer-motion"
+import { FaGithub, FaInstagram, FaLinkedin, FaLinkedinIn, FaYoutube } from "react-icons/fa"
+import { FlickeringGrid } from "../ui/dot-pattern"
 
 
 export default function Home() {
@@ -44,7 +46,7 @@ export default function Home() {
   let xPercent = 0;
   let direction = -1;
 
-  useEffect( () => {
+  useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
     gsap.to(slider.current, {
       scrollTrigger: {
@@ -60,39 +62,101 @@ export default function Home() {
   }, [])
 
   const animate = () => {
-    if(xPercent < -100){
+    if (xPercent < -100) {
       xPercent = 0;
     }
-    else if(xPercent > 0){
+    else if (xPercent > 0) {
       xPercent = -100;
     }
-    gsap.set(firstText.current, {xPercent: xPercent})
-    gsap.set(secondText.current, {xPercent: xPercent})
+    gsap.set(firstText.current, { xPercent: xPercent })
+    gsap.set(secondText.current, { xPercent: xPercent })
     requestAnimationFrame(animate);
     xPercent += 0.10 * direction;
   }
 
 
- 
+
   return (
     <>
       <div className={`${styles.container} h-[60vh] xl:h-[70vh] `}>
-        <div className={` ${styles.body} px-4  text-3xl md:text-5xl font-bold`}>
-          <p>  Pawan Sharma</p>
-          <p>  CREATIVE DEVELOPER</p>
 
-
-          <p className="text-sm md:text-base lg:text-lg xl:text-xl text-center mt-2 md:mt-3  text-gray-300  max-w-3xl mx-auto  tracking-wider ">
-            Freelance Full Stack Developer • Next.js • Nest.js • UI/UX Enthusiast • Learning SQL & Python | Open to
-            Internships & New Opportunities
-          </p>
-
-          <div className="max-w-xl lg:max-w-2xl mx-auto flex p-4 gap-1  lg:gap-4">
-            <Image src="/pawan1.png" alt="pawan image" width={60} height={60} className="rounded-full h-[40px] w-[40px] xl:h-[45px] xl:w-[45px]"></Image>
-            <p className="text-sm md:text-base lg:text-lg xl:text-xl text-center mb-1 md:mb-3  normal-case tracking-wider max-w-2xl text-white/95">
-              ❝ I build every project with the same dedication as if it were my own brand. ❞
+        <div className={`${styles.body} h-[60vh] xl:h-[70vh]  max-w-7xl px-6 md:px-10  lg:px-10 flex items-center gap-10 `}>
+          <FlickeringGrid
+            className="absolute  inset-0 mask-[radial-gradient(350px_circle_at_center,white,transparent)]"
+            squareSize={4}
+            gridGap={3}
+            color="#60A5FA"
+            maxOpacity={0.5}
+            flickerChance={0.1}
+          />
+          <div className="lg:max-w-2xl xl:max-w-3xl text-left lg:pt-10">
+            <p className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-sans ">Pawan Sharma</p>
+            <p className="text-3xl md:text-5xl  lg:text-6xl xl:text-7xl  font-sans mt-2">
+              Creative Developer
             </p>
+
+            <p className="text-xs  md:text-sm lg:text-sm xl:text-base font-extralight mt-4 text-gray-200 tracking-wider">
+              I’m a Full-Stack Developer with 2+ year of experience building modern web
+              applications using React, Next.js, and NestJS. I enjoy creating clean,
+              responsive interfaces and scalable backend systems. I’ve worked on several
+              live projects where I collaborated with designers and teams to improve UI/UX
+              and deliver high-performance applications. Currently, I’m expanding my
+              skills in Full-Stack development with Generative AI while exploring new
+              technologies to build smarter and more impactful digital products.
+              <br /> Open to Internships & New Opportunities
+            </p>
+
+            <div className="flex items-center gap-5 mt-6  text-xl md:text-2xl  ">
+              <a
+                href="https://linkedin.com"
+                target="_blank"
+                className="hover:text-blue-400 transition"
+              >
+                <FaLinkedinIn />
+              </a>
+
+              <a
+                href="https://github.com"
+                target="_blank"
+                className="hover:text-gray-300 transition"
+              >
+                <FaGithub />
+              </a>
+
+              <a
+                href="https://instagram.com"
+                target="_blank"
+                className="hover:text-pink-400 transition"
+              >
+                <FaInstagram />
+              </a>
+
+              <a
+                href="https://youtube.com"
+                target="_blank"
+                className="hover:text-red-500 transition"
+              >
+                <FaYoutube />
+              </a>
+            </div>
           </div>
+         
+          {/* RIGHT SIDE IMAGE */}
+       <div className="relative hidden lg:flex 
+                w-full  lg:w-[35%] xl:w-[40%] 
+                items-center justify-center aspect-square">
+
+  <div className="relative z-20 w-full h-full overflow-hidden">
+    <Image
+      src="/pawan1.png"
+      alt="Pawan"
+      fill
+      className="object-cover rounded-[100%_60%_40%_50%]"
+    />
+  </div>
+
+</div>
+
         </div>
         <div className={styles.grid}>
           {windowsWidth > 0 &&
@@ -106,18 +170,18 @@ export default function Home() {
         </div>
       </div>
 
-       <main className=" relative flex h-[10vh]  overflow-hidden  bg-[#191919]">
-     
-      <div className="absolute w-full overflow-hidden">
-    <div ref={slider} className="relative whitespace-nowrap">
-      <p
-        ref={firstText}
-        className="relative m-0 text-white text-[6vh]  md:text-[4vw] font-bold pr-[50px] tracking-wider "
-      >
-  •  Freelance Developer •  Freelance Frontend Developer  •  Freelance Developer •  Freelance Frontend Developer • Web Designer • UI/UX Enthusiast      </p>
-         </div>
-  </div>
-    </main>
+      <main className=" relative flex h-[10vh]  overflow-hidden  bg-[#191919]">
+
+        <div className="absolute w-full overflow-hidden">
+          <div ref={slider} className="relative whitespace-nowrap">
+            <p
+              ref={firstText}
+              className="relative m-0 text-white text-[6vh]  md:text-[4vw] font-bold pr-[50px] tracking-wider "
+            >
+              •  Freelance Developer •  Freelance Frontend Developer  •  Freelance Developer •  Freelance Frontend Developer • Web Designer • UI/UX Enthusiast      </p>
+          </div>
+        </div>
+      </main>
     </>
   )
 }
--- new/app/layout.tsx
+++ new/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <body
         className={`antialiased `}
       >
-        <Navbar/>
+        {/* <Navbar/> */}
         {children}
       </body>
     </html>
